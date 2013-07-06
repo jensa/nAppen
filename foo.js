79af7cdb0978b4d647766f2376756a7713595ff7
Jens Arvidsson <jensarv@gmail.com>
2013-07-06 14:32:47 +0200

    auth

--- foo.js
+++ foo.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express ();
+var flash = require('connect-flash');
+var MongoStore = require('connect-mongo')(express);
 var expressValidator = require('express-validator');
 var router = require ('./requestRouter');
 
@@ -7,8 +9,17 @@ app.set('views', __dirname + '/views');
 app.set('view options', { layout: false });
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session(
+	{
+      secret: 'SUPER SECRET',
+      store: new MongoStore(
+	  {
+        db: 'test'
+      })
+	}));
 app.use(expressValidator ());
+app.use(flash ());
 
 router.setRoutes (app);
 app.listen(8000);
--- requestRouter.js
+++ requestRouter.js
@@ -1,33 +1,97 @@
-var mongz = require ('mongodb');
+﻿var mongoose = require('mongoose');
+var bcrypt = require('bcrypt')
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+	console.log ('opened db connection');
+});
+
+
+passport.use(new LocalStrategy(authUser));
+
+var userSchema = new mongoose.Schema({
+	username : String,
+	password : String
+});
+
+userSchema.pre('save', function(next) {
+	var user = this;
+
+	if(!user.isModified('password')) 
+		return next();
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		if(err) 
+			return next(err);
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			if(err) 
+				return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+var User = mongoose.model ('users', userSchema);
+
+function authUser (username, password, done) {
+	console.log ("authing:"+username+", "+password);
+//	if (username == '' || username == 'Namn')
+//		return done (null, false, {message: 'Måste fylla i användarnamnet'});
+//	if (password == '')
+//		return done (null, false, {message: 'Måste fylla i lösenordet'});
+    User.find({ username: username }).exec(function(err, user) {
+		console.log ("user:"+user+", err: "+err);
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+     // if (!user.validPassword(password)) {
+   //     return done(null, false, { message: 'Incorrect password.' });
+   //   }
+      return done(null, user);
+    });
+ }
 
 function setRoutes (app){
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get ('/event', event);
 app.get ('/login', login);
-app.post ('/login', validateLogin);
+app.post ('/login', auth () );
 app.get ('/news', news);
+app.get ('/makeusr', newuser);
 app.get('/', function (req, res){ res.redirect('/login')});
 //404
 app.get('*', balls);
 }
 
-function login (req, res){
-	if (req.query.failed)
-		res.render ('login.jade', {title:'Login', message:'failed to log in'});
-	res.render ('login.jade', {title:'Login', message:'GO LOG IN!'});
+function newuser (req, res){
+	var user = req.query.user;
+	var pwd = req.query.pwd;
+	console.log ("making new user:"+user+", pass:"+pwd);
+	var newUser = new User ({username: user, password : pwd});
+	console.log ("made user");
+	newUser.save(function (err) {if (err) console.log ('Error on save!')});
+	console.log ("saved");
+	res.redirect('/event');
 }
 
-function validateLogin (req, res){
-	var loginSuccessful = true;
-	req.assert('username', 'Name is required').notEmpty();
-    req.assert('password', 'Password is required').notEmpty();
-	var errors = req.validationErrors();
-	loginSuccessful = !errors; // TODO: auth, here. yeah.
-	if (loginSuccessful) {
-		res.redirect ('/news');
-	} else{
-		console.log (errors);
-		res.redirect('/login?failed=true');
-	}
+function login (req, res){
+	if (req.query.msg=='failedval')
+		res.render ('login.jade', {title:'Login', message:'Ett eller flera fält tomma'});
+	else if (req.query.msg=='failedauth')
+		res.render ('login.jade', {title:'Login', message:'Fel användarnamn eller lösenord'});
+	res.render ('login.jade', {title:'Login'});
+}
+
+function auth (){
+	return passport.authenticate('local', { successRedirect: '/news',
+			failureRedirect: '/event',
+			failureFlash: false });
 }
 
 function event (req, res){
