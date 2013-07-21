8121f7061d3d541770618b3c945e062b480b3ab1
Jens Arvidsson <jensarv@gmail.com>
2013-07-21 18:37:44 +0200

    hata css

--- public/clientscripts/scripts.js
+++ public/clientscripts/scripts.js
@@ -3,3 +3,22 @@ $("#eventblock").click(function(){
 		window.location=$(this).find("a").attr("href"); 
 		return false;
 	});
+
+$(document).ready(function (){
+	resizeCss ();
+});
+
+$(window).resize(function() {
+	resizeCss ();
+});
+
+function resizeCss (){
+	var width = $(window).width();
+	var inputwidth = "100%";
+	if (width > 400)
+		inputwidth = "300px";
+	$("input").css ("width", inputwidth);
+	$("select").css ("width", inputwidth);
+	$("textarea").css ("width", inputwidth);
+
+}
