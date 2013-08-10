b0a2334bf4078af349ee62c64eee1dd1ac1a6080
Jens Arvidsson <jensarv@gmail.com>
2013-08-10 16:43:55 +0200

    ball

--- mods/helper.js
+++ mods/helper.js
@@ -5,7 +5,30 @@ function renderPage (req, res, page, options) {
 		options.dadminrole = true;
 	if (getAdminRole (req))
 		options.adminrole = true;
+	if (req.session.user)
+		options.groupName = getNollegroupName(req.session.user.group);
+	else
+		options.groupName = '';
 	res.render (page, options);
+}
+
+function getNollegroupName (letter){
+	switch (letter){
+		case 'A': return 'and Onion, Sourcream'; break;
+		case 'B': return 'burgarchips, Cheese'; break;
+		case 'C': return 'Chips med tacosmak'; break;
+		case 'D': return 'Dillchips'; break;
+		case 'E': return 'Echo'; break;
+		case 'F': return 'Fucking grillchips'; break;
+		case 'G': return 'Gotländska chips'; break;
+		case 'H': return 'Hotel'; break;
+		case 'I': return 'India'; break;
+		case 'J': return 'Jalapenochips'; break;
+		case 'K': return 'Kilo'; break;
+		case 'L': return 'Lättsaltade chips'; break;
+		case 'M': return 'Mango'; break;
+		case 'ALL': return 'Alla grupper'; break;
+	}
 }
 
 exports.renderPage = renderPage;
